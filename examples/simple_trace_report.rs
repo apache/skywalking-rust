c550f9734dfab7335c053a74cffe4121c51e8817
dkkb <dabkb@aol.com>
2022-01-06 12:33:19 +0800

    Fix examples in documentation and README

--- examples/simple_trace_report.rs
+++ examples/simple_trace_report.rs
@@ -1,13 +1,11 @@
-use skywalking_rust::context::system_time::UnixTimeStampFetcher;
 use skywalking_rust::context::trace_context::TracingContext;
 use skywalking_rust::reporter::grpc::Reporter;
-use std::sync::Arc;
 use tokio;
 
 #[tokio::main]
 async fn main() {
-    let tx = Reporter::start("http://0.0.0.0:11800".to_string()).await;
-    let mut context = TracingContext::default_internal("service", "instance");
+    let tx = Reporter::start("http://0.0.0.0:11800").await;
+    let mut context = TracingContext::default("service", "instance");
     {
         let span = context.create_entry_span("op1").unwrap();
         context.finalize_span(span);
