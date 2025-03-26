a2e2db787ea58f46a7b65a9b978f0493e33db16d
jmjoy <jmjoy@apache.org>
2025-03-26 18:05:19 +0800

    Fix CI

--- e2e/src/main.rs
+++ e2e/src/main.rs
@@ -176,7 +176,7 @@ async fn consumer_response(
 }
 
 async fn run_consumer_service(host: [u8; 4]) {
-    let addr = SocketAddr::from((host, 8081));
+    let addr = SocketAddr::from((host, 8082));
     let listener = TcpListener::bind(&addr).await.unwrap();
     println!("starting consumer on {:?}...", &addr);
 
@@ -255,22 +255,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let mut client_config = RDKafkaClientConfig::new();
     client_config
-        .set("bootstrap.servers", "broker:9092")
-        .set("message.timeout.ms", "6000");
+    .set("bootstrap.servers", "broker:9092")
+    .set("message.timeout.ms", "6000");
     let (reporter2, reporting) = KafkaReportBuilder::new(client_config)
-        .with_err_handle(|message, err| {
-            eprintln!(
-                "kafka reporter failed, message: {}, err: {:?}",
-                message, err
-            );
-        })
-        .build()
-        .await?;
+    .with_err_handle(|message, err| {
+    eprintln!(
+    "kafka reporter failed, message: {}, err: {:?}",
+    message, err
+    );
+    })
+    .build()
+    .await?;
     let handle2 = reporting.spawn();
 
     let reporter = CombineReporter {
-        grpc_reporter: reporter1,
-        kafka_reporter: reporter2,
+    grpc_reporter: reporter1,
+    kafka_reporter: reporter2,
     };
 
     if opt.mode == "consumer" {
